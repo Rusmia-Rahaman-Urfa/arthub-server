84cf96e70abae058d020a82a743bec558e752c10
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-08-29 23:09:24 +0600

    feat: add JWT authentication middleware with JWKS verification

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
+const app = express();
 
 dotenv.config();
-
-const app = express();
 app.use(express.json());
 app.use(cors());
+
+const JWKS = createRemoteJWKSet(
+  new URL(`${process.env.CLIENT_URL_FOR_JWT}/api/auth/jwks`)
+)
+const verifyToken = async (req, res, next) => {
+  const tokenData = req.headers.authorization;
+
+  if (!tokenData) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  };
+  const token = tokenData.split(' ')[1];
+  if (!token) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  };
+
+  try {
+    const { payload } = await jwtVerify(token, JWKS);
+    req.user = payload;  // sent payload to req.user
+    next();
+  }
+  catch (error) {
+    return res.status(403).json({ message: 'Forbidden' });
+  }
+};
+
 
 const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri, {
