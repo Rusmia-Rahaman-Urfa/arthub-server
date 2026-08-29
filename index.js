568ea0f6b810324a322fc042e02f9d1a972d8c59
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-08-29 23:01:15 +0600

    feat: setup server dependencies and base configuration

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server is running on port ${process.env.PORT || 5000}`);
+});
