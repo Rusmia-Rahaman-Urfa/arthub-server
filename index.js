89a2b5f81cf4d53e75d2bba06597abfa6588cb9f
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-08-29 23:04:23 +0600

    feat: implement MongoDB client and database connection setup

--- index.js
+++ index.js
@@ -1,12 +1,32 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
+
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    const ArtHubDB = client.db('ArtHub');
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Client connection open
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World');
