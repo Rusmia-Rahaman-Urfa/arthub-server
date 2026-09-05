1b97108eada0ceb61f58e3356b76e36bfa67bf97
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-09-05 16:34:31 +0600

    feat: create public artwork fetching and filtering APIs

--- index.js
+++ index.js
@@ -78,9 +78,51 @@ async function run() {
     const purchasesCollection = ArtHubDB.collection('purchases');
     const userCollection = ArtHubDB.collection('user');
     const commentCollection = ArtHubDB.collection('comment');
-  } finally {
-    // Client connection open
-  }
+
+    //get artwork for feature section 
+    app.get('/api/artwork/features', async (req, res) => {
+      const result = await artWorksCollection.find().toArray()
+      res.json(result)
+    })
+    // get artworks
+    app.get('/api/artwork', async (req, res) => {
+      const { search, minPrice, maxPrice, category, sort, page = 1, limit = 12 } = req.query;
+      let query = {};
+      if (search) {
+        query.$or = [
+          { title: { $regex: search, $options: 'i' } },
+          { artist: { $regex: search, $options: 'i' } },
+        ]};
+        if (minPrice) query.price = { $gte: Number(minPrice) };
+        if (maxPrice) query.price = { $lte: Number(maxPrice) };
+        if (category && category !== 'All') {
+          query.category = category.toLowerCase();
+        }
+        let sortOption = {};
+        if (sort === 'Latest') {
+          sortOption = { createAt: -1 }
+        } else if (sort === 'Oldest') {
+          sortOption = { createAt: 1 }
+        } else if (sort === 'Price Low-High') {
+          sortOption = { price: 1 }
+        } else if (sort === 'Price High-Low') {
+          sortOption = { price: -1 }
+        }
+        const skip = (Number(page) - 1) * Number(limit);
+        const result = await artWorksCollection.find(query).skip(skip).limit(Number(limit)).sort(sortOption).toArray();
+        const totalData = await artWorksCollection.countDocuments(query);
+        const totalPage = Math.ceil(totalData / Number(limit));
+        res.json({ data: result, page: Number(page), totalPage });
+      });
+      //get artwork by id
+      app.get('/api/artwork/:id', async (req, res) => {
+        const { id } = req.params;
+        const result = await artWorksCollection.findOne({ _id: new ObjectId(id) });
+        res.json(result);
+        });
+  
+}finally {
+  await client.close();
 }
 run().catch(console.dir);
 
@@ -90,4 +132,4 @@ app.get('/', (req, res) => {
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server is running on port ${process.env.PORT || 5000}`);
-});
+})};
